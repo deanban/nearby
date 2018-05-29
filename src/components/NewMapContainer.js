9a9b62b2e318c83b2971732e79184aa7fbe724fe
github username <github email address>
2018-05-29 18:28:29 -0400

    change & submit

--- src/components/NewMapContainer.js
+++ src/components/NewMapContainer.js
@@ -1,43 +1,45 @@
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React, { Component } from 'react';
 
-
 export class NewMapContainer extends Component {
 
-	state = {
-    	lat:0,
-    	lng:0
-  }
 
 
-  getLocation = () => {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(this.success)
-      } else {
-          console.log("Geolocation is not supported by this browser.")
-      }
-  }
-  success = (pos) => {
-      console.log(pos.coords)
-      this.setState({
-        lat: pos.coords.latitude, lng: pos.coords.longitude
-      }, () => console.log(this.state));
-  }
+	// state = {
+ //    	lat:0,
+ //    	lng:0
+ //  }
 
+
+  // getLocation = () => {
+  //     if (navigator.geolocation) {
+  //         navigator.geolocation.getCurrentPosition(this.success)
+  //     } else {
+  //         console.log("Geolocation is not supported by this browser.")
+  //     }
+  // }
+  // success = (pos) => {
+  //     console.log(pos.coords)
+  //     this.setState({
+  //       lat: pos.coords.latitude, lng: pos.coords.longitude
+  //     }, () => console.log(this.state));
+  // }
 
   render() {
-  	this.getLocation()
+    console.log(this.props)
     return (
+      <div>
       <Map google={this.props.google} zoom={14}
-      center={{ lat: this.state.lat, lng: this.state.lng }}
+      center={{ lat: this.props.lat, lng: this.props.lng }}
       >
 
         <Marker
 	        name="Current Location"
-	        position={{ lat: this.state.lat, lng: this.state.lng }}
+	        position={{ lat: this.props.lat, lng: this.props.lng }}
 	        title="The marker`s title will appear as a tooltip."
       />
       </Map>
+      </div>
     );
   }
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,25 +1,58 @@
 import React from 'react'
+import NewMapContainer from './NewMapContainer'
 
 export default class Searchbar extends React.Component{
 
-	componentDidMount(){
-		fetch("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=48.859294,2.347589&radius=5000&type=park&key=AIzaSyAF3laRwdxS7LqBHaCP5UbQX-ZKOOTFPwE")
-		.then(resp => resp.json())
-		.then((json) => {
-
-		})
+	state = {
+		searchStr: "",
+		currentLat:0,
+		currentLng:0,
 
 	}
+
+	componentWillMount() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(this.success)
+      } else {
+          console.log("Geolocation is not supported by this browser.")
+      }
+  	}
+
+	success = (pos) => {
+		// console.log(pos.coords)
+	    this.setState({
+	      lat: pos.coords.latitude, lng: pos.coords.longitude
+	    });
+	}
+
+	handleChange = (event) => {
+		console.log(event.target.value)
+		this.setState({
+			searchStr: event.target.value
+		})
+	}
+
+	handleSubmit = (event) =>{
+		event.preventDefault()
+		debugger
+		fetch(`https://maps.googleapis.com/maps/api/place/radarsearch/json?location=${this.state.currentLat},${this.state.currentLng}&radius=50&type=${this.state.searchStr}&key=AIzaSyAF3laRwdxS7LqBHaCP5UbQX-ZKOOTFPwE`)
+		.then(resp => resp.json())
+
+
+
+	}
+
 	 render(){
+	 	// this.getLocation()
 		  return(
+
 				<div className="search" >
-					<form>
+					<form onSubmit={this.handleSubmit}>
+			          <input type="text" placeholder="search here" onChange={this.handleChange}/>
 
-
-			          <input type="text" placeholder="search here"/>
-
-			        <input type="submit" value="Submit" />
+			        <input type="submit" value="Submit"/>
 			      </form>
+			    <NewMapContainer lat={this.state.lat} lng={this.state.lng}/>
 				</div>
 		  );
 	 }
